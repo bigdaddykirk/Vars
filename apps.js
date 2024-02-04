6bfc72ac727b300440da903b9891ca4b5a93b76c
Kirk Springs <kspringsjr@gmail.com>
2024-02-04 15:52:55 -0500

    Vars

---
+++ apps.js
@@ -0,0 +1,29 @@
+var car = 20;
+
+var crash = 40;
+
+function addCrash(crash,car){
+    return crash + car
+}
+
+console.log(addCrash(crash,car))
+
+
+
+function subCar(car,crash){
+    return car - crash
+}
+
+console.log(subCar(car,crash))
+
+function mulCrash(car,crash){
+    return car * crash
+}
+
+console.log(mulCrash(car,crash))
+
+function divCar(car,crash){
+    return car / crash
+}
+
+console.log(divCar(car,crash))
